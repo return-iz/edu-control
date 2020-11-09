211012bea00b2fcaa421de0f1b4200c1efce34d9
Igor Zhigulin <igor.iz.zhigulin@gmail.com>
2020-11-09 23:52:18 +0700

    Новая структура проекта
    Поменял структуру проекта. Переосмыслил подход. Осознал, что делать все на WinAPI сложно, но при этом решил сделать все на OpenGL с использованием glfw, glad и, может быть, других библиотек. Посмотрим, что из этого выйдет

--- EduControl/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <Windows.h>
-
-int main()
-{
-	std::cout << "Hello world\n";
-}
---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        return -1;
+    }
+
+    glClearColor(1, 1, 1, 1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
